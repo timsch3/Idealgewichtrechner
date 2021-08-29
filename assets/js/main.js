3edf925ea88a38657ba125c78ad4387875ed5248
timsch3 <timsch3@gmail.com>
2021-08-29 14:55:36 +0200

    fixed result output formatting

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ function calc() {
     if (height > 100 && age > 12) {
         let broad = true ? document.getElementById('broad').checked : document.getElementById('thin').checked
         let result = broad ? ((height - 100) + (age / 10)) * 0.9 * 1.1 : ((height - 100) + (age / 10)) * 0.9 * 0.9
-        document.getElementById('output').innerHTML = `Dein Idealgewicht beträgt ${result.toFixed(0)}kg.<br><br><strong>Aber denk dran:</strong> du bist schön,<br>egal wie viel du wiegst!`
+        document.getElementById('output').innerHTML = `Dein Idealgewicht beträgt ${result.toFixed(1).replace('.', ',')} kg.<br><br><strong>Aber denk dran:</strong> du bist schön,<br>egal wie viel du wiegst!`
     }
     else {
         alert('Dieser Test ist doof!')
